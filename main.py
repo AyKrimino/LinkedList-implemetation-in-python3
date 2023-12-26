4d7462977c9998141f7878c87dd55a045c7a5061
AyKrimino <ayoub.krimi14@gmail.com>
2023-12-26 13:39:51 +0100

    I finally fixed the error of the infinite loop
    reason are I cannot append the same node twice to the linked list because each node points to its next node by its reference (adress) and if we have the same node appended twice we gonna have a problem

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 class Node:
-    """
+    '''
     
-    Node class represent the element of the linked list
-    """
+        Node class represent the element of the linked list
+    '''
     def __init__(self, value=None):
         self.value = value
         self.next = None
 
 
 class LinkedList:
+    '''
+    
+    Note:
+        You cannot have the same node appended twice to the LinkedList
+        but you can instead append different nodes that have the same value
+        the reason is that each node have an address in memory and each node
+        in our LinkedList should point to its next node by its address and then 
+        if we have the same node appended twice we gonna have an infinite loop 
+        when we iterate over the LinkedList
+    '''
+        
     def __init__(self):
         self.head = None
         self.length = 0
@@ -20,11 +31,11 @@ class LinkedList:
         return self.head is None
 
     def append(self, node):
-        """
+        '''
 
-        append the node at the end of the linked list
-        node: type Node: instance of Node class
-        """
+            append the node at the end of the linked list
+            node: type Node: instance of Node class
+        '''
         if self.empty():  # Edge case : when the append method is called to append the head node
             self.head = node
         else:
@@ -36,30 +47,31 @@ class LinkedList:
         self.length += 1
          
     def append_left(self, node):
-        """
-        
-        append the node at the start of the linked list
-        node:type Node: instance of Node class
-        """
+        '''
+            append the node at the start of the linked list
+        Args:
+            node (Node): instance of the Node class implemented above
+        '''
         node.next = self.head
         self.head = node
     
         self.length += 1
     
     def append_at(self, node, index):
-        """
+        '''
 
         Args:
             node (Node): instance of the Node class implemented above
-            idx (int): represents the index where the node should be inserted and it shouldn't be out of range of the linked list instance
+            idx (int): represents the index where the node should be inserted 
+                        and it shouldn't be out of range of the linked list instance
         Note:
             LinkedList is 0-BASED indexed
-        """
-        if not (0 <= index < len(self)):
+        '''
+        if not (0 <= index <= len(self)):
             raise IndexError('Linked list index out of range')
         elif index == 0:  # if we want to make the node as a head node we should simply call the append_left() method implemented above
             self.append_left(node)
-        elif index == len(self) - 1:
+        elif index == len(self):
             self.append(node)  # if we want to append at the last index of the linked list then we should simply call the append() method implemented above
         else:
             current_node = self.head
@@ -80,8 +92,10 @@ class LinkedList:
             print('Underflow occurred! LinkedList is empty.')
 
         current_node = self.head
+        i = 0
 
         while True:  # we guarantee that infinite loop gonna stop when we reach the last element of our LinkedList
+            i += 1
             if current_node.next is None: # if we reach the last element we would print it and then break the infinite loop
                 print(current_node.value)
                 break
@@ -96,13 +110,14 @@ if __name__ == '__main__':
     node3 = Node('hamadi')
     node4 = Node('fedi')
     node5 = Node('chedi')
+    node6 = Node('fedi')
     linked_list1 = LinkedList()
     linked_list1.append(node1)
     linked_list1.append(node2)
     linked_list1.append(node3)
     linked_list1.append(node4)
     linked_list1.append_left(node5)
-    # linked_list1.append_at(node4, len(linked_list1) - 1)
+    linked_list1.append_at(node6, 3)
     linked_list1.print_linked_list()  # lists all the elements of the linked_list1
     print(len(linked_list1))  # prints the length of the linked_list2 using the dunder method __len__ defined in the class
 
